43bf17f9918eb946687af276a4ff2513b596dce3
aaronburmeister <f34r91@gmail.com>
2020-06-19 12:41:03 -0600

    Add CreatePhoneNumber.js

---
+++ javascript/6kyu/CreatePhoneNumber.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/525f50e3b73515a6db000b83
+
+function createPhoneNumber(n){
+    return `(${n[0]}${n[1]}${n[2]}) ${n[3]}${n[4]}${n[5]}-${n[6]}${n[7]}${n[8]}${n[9]}`
+}
+
+// June 15, 2020
